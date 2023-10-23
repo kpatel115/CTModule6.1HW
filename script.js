33ddc61d522965fff794f5923dffa5810fd2bdc7
kpatel114 <kpatel114@hawk.iit.edu>
2023-10-23 12:07:25 -0600

    completed 4 codewar questions and got correct output for 2 exercises

---
+++ script.js
@@ -0,0 +1,156 @@
+// Codewars Question 1
+// Is this a triangle
+/*
+def is_triangle(a, b, c):
+    if (a + b) <= c:
+        return False
+    elif (b + c) <= a:
+        return False
+    elif (a + c) <= b:
+        return False
+    else:
+        return True 
+*/
+console.log("Codewars Question 1")
+function isTriangle(a,b,c){
+    if ((a + b) <= c){
+        console.log('False')
+    } else if ((b + c) <= a){
+        console.log('False')
+    } else if ((a + c) <= b){
+        console.log('False')
+    } else {
+        console.log('True')
+    }
+}
+isTriangle(4,2,3)
+
+// Codewars Question 2
+// String Repeat
+/* - Python
+def repeat_str(repeat, string):
+    r = int(repeat)
+    s = str(string)
+    str1 = s * r
+    return str1;
+*/
+// JS
+console.log("Codewars Question 2")
+function repeatStr(int_repeat, string_repeat){
+    let n = parseInt(int_repeat)
+    let str2 = ""
+    for (let i = 0; i < n; i++){
+      str2 += string_repeat
+    }
+  console.log(str2)
+}
+repeatStr(6, "I")
+
+
+// Codewars Question 3
+// Friend or Foe
+/* Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+i.e.*/
+/* - Python
+def friend(x):
+    for name in x[:]:
+        if len(name) == 4:
+            continue
+        else:
+            x.remove(name)
+    return x
+    
+    #Code
+*/
+console.log("Codewars Question 3")
+people = ["Ryan", "Kieran", "Jason", "Yous"]
+function friend(lst){
+    let friends = []
+    for (let i = 0; i < lst.length; i++){
+        if (lst[i].length == 4){
+            friends.push(lst[i])
+        } else {
+            console.log(`${lst[i]} is not my friend`)
+        }
+    }
+    console.log(friends)
+}
+friend(people)
+
+// Codewars Question 4
+// Reverse List Order
+
+/* In this kata you will 
+create a function that takes in a list
+and returns a list with the reverse order. */
+ex1 = [1, 2, 3, 4]
+ex2 = [9, 2, 0, 7]
+function revList(lst){
+    reversed = lst.reverse()
+    console.log(reversed)
+}
+revList(ex2)
+
+//==================Exercise #1 ==========//
+/*Write a function that takes in the string and the list of dog names, loops through 
+the list and checks that the current name is in the string passed in. The output should be:
+"Matched dog_name" if name is in the string, if no matches are present console.log "No Matches"
+*/
+let dog_string = "Hello Max, my name is Dog, and I have purple eyes!"
+let dog_names = ["Max","HAS","PuRple","dog"]
+let names = []
+
+function findWords(str,lst){
+    //Your code goes here
+    let regex = new RegExp(dog_names.join('|'));
+    for (let i in dog_string) {
+        dog_string[i] = dog_string[i].replace(/\s/g, '');
+    }
+    for (const string of dog_string) {
+        if (string.match(regex)) {
+            console.log('matched')
+        } else {
+            console.log("No match")
+        }
+    }
+}
+
+function findWords2(s,l){
+    for (let i = 0; i < l.length; i++){
+        if (l[i].indexOf(s)){
+            console.log('match')
+        } else {
+            console.log('no match')
+        }
+    }
+}
+//Call method here with parameters
+
+
+findWords2(dog_string, dog_names)
+
+//============Exercise #2 ============//
+/*Write a fucntion that takes in an array and removes every even index with a splice,
+and replaces it with the string "even index" */
+
+let arr1 = ["Max","Baseball","Reboot","Goku","Trucks","Rodger"]
+
+function replaceEvens(arr){
+    //code goes here
+    for (let i = 0; i < arr.length; i++){
+        if (i % 2 == 0){
+            arr.splice(i, 1, 'even index');
+        } else {
+            continue
+        }
+    }
+    console.log(arr)
+}
+replaceEvens(arr1)
+//Expected output
+//Given arr == ["Max","Baseball","Reboot","Goku","Trucks","Rodger"]
+//Output arr == ["even index","Baseball","even index","Goku","even index","Rodger"]
